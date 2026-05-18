3b338aec76cfc4042721b31b38d2d97167193a37
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-18 15:22:19 +0600

    update arraylist.java

--- src/Arraylist/Arraylist.java
+++ src/Arraylist/Arraylist.java
@@ -1,6 +1,7 @@
 package Arraylist;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Arraylist {
 
@@ -15,12 +16,32 @@ public class Arraylist {
 
         System.out.println("Names in the ArrayList: " + names);
 
+        names.add(1, "mahzabin");
+        System.out.println("Names after adding new name: " + names);
 
         String firstName = names.get(0);
         System.out.println("First name: " + firstName);
+        System.out.println("Names after adding new name at index 1: " + names.get(1));
+        names.set(1, "Bob");
+        System.out.println("Names after updating name at index 1: " + names.get(1));
 
-     
+
         names.remove(1);
+        names.remove("Alice");
+        System.out.println("Names after removal: " + names);
+
+        names.add("Bob");
+        names.add("Charlie");
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+
+        names.add("Bobo");
+        names.add("DCharlie");
+        Collections.sort(names);
+        System.out.println("Names after sorting: " + names);
+        names.clear();
         System.out.println("Names after removal: " + names);
     }
     }
