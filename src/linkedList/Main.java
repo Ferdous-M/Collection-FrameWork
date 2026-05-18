f70f80278d2cb25d1e8d0cf32fd8b87c5359c83a
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-18 16:01:07 +0600

    basic of linked list

---
+++ src/linkedList/Main.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        list.addFirst(5);
+        list.addLast(6);
+        System.out.println(list);
+}
+}
