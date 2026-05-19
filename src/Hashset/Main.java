b5271473e181dd405f6c4ba4128195b145948e7f
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-19 16:33:05 +0600

    more

--- src/Hashset/Main.java
+++ src/Hashset/Main.java
@@ -4,19 +4,21 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet<String> hashSet = new HashSet<String>();
+        HashSet<Integer> hashSet = new HashSet<Integer>();
 
-        hashSet.add("a");
-        hashSet.add("b");
-        hashSet.add("c");
-        hashSet.add("a"); // Duplicate, will not be added
-        hashSet.add("b");
+        hashSet.add(1);
+        hashSet.add(2);
+        hashSet.add(3);
+        hashSet.add(3); // Duplicate, will not be added
+        hashSet.add(2);
 
+        for(Integer element : hashSet) {
+            System.out.println(element);
+        }
+
+        hashSet.remove(3);
         System.out.println(hashSet);
-
-        hashSet.remove("c");
-        System.out.println(hashSet);
-        System.out.println(hashSet.contains("a"));
+        System.out.println(hashSet.contains(3));
         System.out.println(hashSet.size());
         System.out.println(hashSet.isEmpty());
         hashSet.clear();
