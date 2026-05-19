327e69ad2344b124701c43db1d78e91628faa3c6
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-19 16:51:29 +0600

    priority queue

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -1,23 +1,22 @@
 package Queue;
 
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> pq = new PriorityQueue<>();
 
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
 
-        System.out.println(queue);
+        System.out.println(pq);
 
-        queue.remove();
-
-        System.out.println(queue);
-
-        System.out.println(queue.peek());
+        while(!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
     }
 }
