483268274343df9613136157d6e9446ec887e705
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-19 16:48:19 +0600

    TreeMap

---
+++ src/Map/TreeMapDemo.java
@@ -0,0 +1,16 @@
+package Map;
+
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> map = new TreeMap<>();
+
+        map.put(3, "C");
+        map.put(1, "A");
+        map.put(2, "B");
+
+        System.out.println(map);
+    }
+}
